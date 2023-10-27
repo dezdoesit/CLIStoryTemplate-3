70013d74a92f64ec149ac9e1fb54d7ae973aee60
caleb <goodec740@gmail.com>
2023-10-27 15:28:14 -0400

    lil add on

--- CLIStoryTemplate/Chapter 2/Chapter2.swift
+++ CLIStoryTemplate/Chapter 2/Chapter2.swift
@@ -9,3 +9,8 @@ func chapterTwo() {
     // Your portion of the story goes here
 print("Humans are revolting and there is a war against the technological revolution. ")
 }
+
+func ambush(){
+    print("Humans plan a swift ambush to catch the robots off gaurd")
+    print("Initially the ambush is a success, but agai the streght of the robots is too much ")
+}
