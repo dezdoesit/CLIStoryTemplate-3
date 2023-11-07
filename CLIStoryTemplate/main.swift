73b998a612567a63364a55810da15ae273ec5eff
dezdoesit <dez.blair@icloud.com>
2023-11-07 16:57:42 -0500

    Changed Main and Chap 3 11/7

--- CLIStoryTemplate/main.swift
+++ CLIStoryTemplate/main.swift
@@ -6,11 +6,25 @@
 import Foundation
 
 func readStory() {
+    print("Generation Tech: \n")
+    print("Written By: Caleb, Denard, Dez, Maya, Natalie \n")
+    print("Chapter 1:")
     chapterOne()
+    print("\n")
+    print("Chapter 2:")
     chapterTwo()
+    print("\n")
+    print("Chapter 3:")
     chapterThree()
+    print("\n")
+    print("Chapter 4:")
     chapterFour()
+    print("\n")
+    print("Chapter 5:")
+
     chapterFive()
+    print("\n")
+
 }
 
 readStory()
