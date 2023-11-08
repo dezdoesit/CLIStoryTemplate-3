c51b3b8ce27d7f9a304594db070413f458138203
dezdoesit <dez.blair@icloud.com>
2023-11-08 13:37:29 -0500

    Last minute changes harharhar

--- CLIStoryTemplate/Chapter 3/Chapter3.swift
+++ CLIStoryTemplate/Chapter 3/Chapter3.swift
@@ -7,20 +7,24 @@ import Foundation
 
 func chapterThree() {
     
+    let citiesAttack = ["Boulder", "Cupertino", "Seattle", "Austin", "New York"]
     let humName: [String] = ["Dez","Maya","Denard","Caleb","Natalie", "Tk", "Asif", "Syeda", "Ghaida", "Charlene", "Adebola", "Baye", "Gayland", "Daniel"]
     let robName: [String] = ["AX-12","1C-12","FX-55"]
-
+    let randomCity = citiesAttack.randomElement() ?? "Detroit"
+    
+    print("It all started in \(randomCity)... The humans begin to fight back and a battle ensues.")
+    
     enum Faction {
         case human
         case robot
     }
-
+    
     struct Soldier {
         var name: String
         var power: Int
         var faction: Faction
     }
-
+    
     struct Platooon {
         var soldiers: [Soldier]
         var platoonName: String
@@ -31,7 +35,7 @@ func chapterThree() {
     //creating empty array of Soldiers & assign var for total power (Robots)
     var robotSoldiers = [Soldier]()
     var powah = 0
-
+    
     //assign a name as well as power level
     for name in humName {
         let newSoldier = Soldier(name: name, power: Int.random(in: 1...10), faction: .human)
@@ -40,7 +44,7 @@ func chapterThree() {
     for newSoldier in humanSoliders {
         hpowah += newSoldier.power
     }
-
+    
     //assign name to robots & PwrLvl
     for name in robName {
         let newRSoldier = Soldier(name: name, power: Int.random(in: 10...50), faction: .robot)
@@ -49,15 +53,15 @@ func chapterThree() {
     for newRSoldier in robotSoldiers {
         powah += newRSoldier.power
     }
-
+    
     var survivorGroupBravo: Platooon = Platooon(soldiers: humanSoliders, platoonName: "Survivor Group bravo")
     var robotPlatoonBravo: Platooon = Platooon(soldiers: robotSoldiers, platoonName: "Robot Platoon bravo")
-
+    
     func revolution(){
-//        var huNames = survivorGroupBravo.soldiers
-//        var robNames = robotPlatoonBravo.soldiers
-//        print(huNames)
-//        print(robNames)
+        var huNames = survivorGroupBravo.soldiers
+        var robNames = robotPlatoonBravo.soldiers
+        //        print(huNames)
+        //        print(robNames)
         
         if hpowah > powah{
             print("The Humans win this battle by \(hpowah - powah) power!")
@@ -66,6 +70,9 @@ func chapterThree() {
         }
         
     }
+    
+    
     revolution()
-
+    print("Is what the news report said...Though everywhere else in the world, the battle continues, and the true winner has yet to prevail.")
+    
 }
